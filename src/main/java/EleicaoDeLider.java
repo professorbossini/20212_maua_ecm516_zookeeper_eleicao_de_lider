a85c5930c2405cc60676b45762d335fc7c61e320
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-28 15:10:57 -0300

    Watches.
    Retomar logo depois da figura 2.3.1 da apostila 03. Registramos um watch com exists e testamos delete e create no zkCli.

--- src/main/java/EleicaoDeLider.java
+++ src/main/java/EleicaoDeLider.java
@@ -1,22 +1,61 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
 import static org.apache.zookeeper.Watcher.Event.EventType.None;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class EleicaoDeLider {
 
     private static final String HOST = "localhost";
     private static final String PORTA = "2181";
     private static final int TIMEOUT = 5000;
+    private static final String NAMESPACE_ELEICAO = "/eleicao";
+    private static final String ZNODE_TESTE_WATCH = "/teste_watcher";
+    private String nomeDoZNodeDesseProcesso;
     private ZooKeeper zooKeeper;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public void realizarCandidatura () throws InterruptedException, KeeperException {
+        String prefixo = String.format("%s/cand_", NAMESPACE_ELEICAO);
+        String pathInteiro = zooKeeper.create(prefixo, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        this.nomeDoZNodeDesseProcesso = pathInteiro.replace(String.format("%s/", NAMESPACE_ELEICAO), "");
+        System.out.println(this.nomeDoZNodeDesseProcesso);
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         System.out.println("Método main: " + Thread.currentThread().getName());
         EleicaoDeLider eleicaoDeLider = new EleicaoDeLider();
         eleicaoDeLider.conectar();
+        eleicaoDeLider.realizarCandidatura();
+        eleicaoDeLider.elegerOLider();
+        eleicaoDeLider.registrarWatcher();
         eleicaoDeLider.executar();
         eleicaoDeLider.fechar();
+    }
+
+    private class TesteWatcher implements Watcher{
+        @Override
+        public void process(WatchedEvent event) {
+            System.out.println(event);
+            switch(event.getType()){
+                case NodeCreated:
+                    System.out.println("ZNode criado");
+                    break;
+                case NodeDeleted:
+                    System.out.println("ZNode removido");
+                    break;
+            }
+        }
+    }
+
+    public void registrarWatcher() throws InterruptedException, KeeperException {
+        TesteWatcher watcher = new TesteWatcher();
+        Stat stat = zooKeeper.exists(ZNODE_TESTE_WATCH, watcher);
+        //znode existe
+        if (stat != null){
+
+        }
     }
 
     public void fechar () throws InterruptedException{
@@ -27,6 +66,25 @@ public class EleicaoDeLider {
         synchronized (zooKeeper){
             zooKeeper.wait();
         }
+    }
+
+    public void elegerOLider () throws InterruptedException, KeeperException {
+        //obter a lista de filhos do ZNode /eleicao
+        //usar o zooKeeper
+        List <String> filhos = zooKeeper.getChildren(NAMESPACE_ELEICAO, false);
+        //ordenar a lista de filhos
+        Collections.sort(filhos);
+        //Verificar o primeiro da lista é igual ao atual nomeDoZNodeDesseProcesso
+        //se for, declarar-se lider. Caso contrário, dizer quem é o lider
+        String lider = filhos.get(0);
+        if (lider.equals(nomeDoZNodeDesseProcesso)){
+            System.out.printf("Me chamo %s e sou o líder.\n", nomeDoZNodeDesseProcesso);
+        }
+        else{
+            System.out.printf("Não sou o líder. O líder é o %s.\n", lider);
+        }
+
+
     }
 
     public void conectar () throws IOException {
